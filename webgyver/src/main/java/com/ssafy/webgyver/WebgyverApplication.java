69cbcdae2015623b998ed5efa67f84984af1d9f5
Dev-Jun125 <dev.jun125@gmail.com>
2023-01-16 15:18:36 +0900

    [ADD] : 백엔드 프로젝트 생성

---
+++ webgyver/src/main/java/com/ssafy/webgyver/WebgyverApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.webgyver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebgyverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebgyverApplication.class, args);
+	}
+
+}
