bb55733e12db4a821fcbe967297704777a7da1d8
YoonHwe <dbsghl123@naver.com>
2023-01-27 14:47:41 +0900

    [MOD] : 예약상담 시간 선택 Recoil 관리로 수정

--- frontend/src/pages/reservation/Reservation.jsx
+++ frontend/src/pages/reservation/Reservation.jsx
@@ -1,14 +1,69 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-horizontal-datepicker';
 import styled from 'styled-components';
 import { useRecoilState, useRecoilValue } from 'recoil';
+import Button from '@mui/material/Button';
 import LocateModal from '../../components/sitepopup/LocateModal';
-import { locateValueState, reservationDate } from '../../atom';
+import {
+  locateValueState,
+  reservationDate,
+  chosenReservation,
+} from '../../atom';
 import StoreInfo from './elements/StoreInfo';
 
 export default function Reservation() {
   const [date, setDate] = useRecoilState(reservationDate);
-  const reservatedDate = useRecoilValue(reservationDate);
+  const [reservation, setReservation] = useRecoilState(chosenReservation);
+  const [clickedTimeButton, setClickedTimeButton] = useState(null);
+  const [reservationButton, setReservationButton] = useState(false);
+  const handleClickedTimeButton = (event, selected) => {
+    // 새로운 데이터 생성
+    const data = { ...selected, date };
+    if (
+      // eslint-disable-next-line operator-linebreak
+      data.idx !== reservation.idx ||
+      // eslint-disable-next-line operator-linebreak
+      data.time !== reservation.time ||
+      // eslint-disable-next-line operator-linebreak
+      data.date.getFullYear() !== reservation.date.getFullYear() ||
+      // eslint-disable-next-line operator-linebreak
+      data.date.getMonth() !== reservation.date.getMonth() ||
+      // eslint-disable-next-line operator-linebreak
+      data.date.getDate() !== reservation.date.getDate()
+    ) {
+      //   alert('신규 등록해야 해!');
+      if (clickedTimeButton !== null) {
+        clickedTimeButton.style.backgroundColor = '#ffffff';
+        clickedTimeButton.style.color = '#1976D2';
+      }
+      setClickedTimeButton(() => event.target);
+      setReservation(data);
+      return;
+    }
+    // alert('일치하는거 또 눌렀네!');
+    clickedTimeButton.style.backgroundColor = '#ffffff';
+    clickedTimeButton.style.color = '#1976D2';
+    setClickedTimeButton(null);
+    setReservation({
+      idx: null,
+      storeName: null,
+      date: null,
+      time: null,
+    });
+  };
+  useEffect(() => {
+    if (clickedTimeButton !== null) {
+      clickedTimeButton.style.backgroundColor = '#1976D2';
+      clickedTimeButton.style.color = '#ffffff';
+    }
+  }, [clickedTimeButton]);
+  useEffect(() => {
+    if (clickedTimeButton === null) {
+      setReservationButton(false);
+    } else {
+      setReservationButton(true);
+    }
+  }, [clickedTimeButton]);
   const location = useRecoilValue(locateValueState);
   const storeList = [
     {
@@ -38,10 +93,92 @@ export default function Reservation() {
         '12:30',
         '12:45',
       ],
-      noTime: ['09:15', '10:15'],
+      noTime: ['09:15', '10:15', '09:30'],
     },
     {
       idx: 2,
+      storeName: '김순자가게',
+      personName: '김순자',
+      address: '대전 서구 청사로 253',
+      detailAddress: '111동 2222호',
+      distance: 312.4,
+      star: 1.2,
+      picture: 'asdf/asdf/sfnias.png',
+      allTime: ['09:00', '09:15', '09:30', '09:45', '10:00', '10:15'],
+      noTime: ['12:15', '10:15', '09:15'],
+    },
+    {
+      idx: 3,
+      storeName: '박복자가게',
+      personName: '박복자',
+      address: '대전 서구 청사로 253',
+      detailAddress: '111동 2222호',
+      distance: 12.4,
+      star: 4.7,
+      picture: 'asdf/asdf/asdf.png',
+      allTime: [
+        '09:00',
+        '09:15',
+        '09:30',
+        '09:45',
+        '10:00',
+        '10:15',
+        '10:30',
+        '10:45',
+        '11:00',
+        '11:15',
+        '11:30',
+        '11:45',
+        '12:00',
+        '12:15',
+        '12:30',
+        '12:45',
+      ],
+      noTime: ['09:15', '10:15', '09:30'],
+    },
+    {
+      idx: 4,
+      storeName: '김순자가게',
+      personName: '김순자',
+      address: '대전 서구 청사로 253',
+      detailAddress: '111동 2222호',
+      distance: 312.4,
+      star: 1.2,
+      picture: 'asdf/asdf/sfnias.png',
+      allTime: ['09:00', '09:15', '09:30', '09:45', '10:00', '10:15'],
+      noTime: ['12:15', '10:15', '09:15'],
+    },
+    {
+      idx: 5,
+      storeName: '박복자가게',
+      personName: '박복자',
+      address: '대전 서구 청사로 253',
+      detailAddress: '111동 2222호',
+      distance: 12.4,
+      star: 4.7,
+      picture: 'asdf/asdf/asdf.png',
+      allTime: [
+        '09:00',
+        '09:15',
+        '09:30',
+        '09:45',
+        '10:00',
+        '10:15',
+        '10:30',
+        '10:45',
+        '11:00',
+        '11:15',
+        '11:30',
+        '11:45',
+        '12:00',
+        '12:15',
+        '12:30',
+        '12:45',
+      ],
+      noTime: ['09:15', '10:15', '09:30'],
+    },
+    {
+      idx: 6,
       storeName: '김순자가게',
       personName: '김순자',
       address: '대전 서구 청사로 253',
@@ -115,9 +252,9 @@ export default function Reservation() {
       <DateDiv>
         <h2 style={{ fontSize: '24px', fontWeight: 'bold' }}>
           날짜 선택
-          {` [ ${reservatedDate.getFullYear()}-${
-            reservatedDate.getMonth() + 1
-          }-${reservatedDate.getDate()} (${reservatedDate.getDay()})] `}
+          {` [ ${date.getFullYear()}-${
+            date.getMonth() + 1
+          }-${date.getDate()} (${date.getDay()})] `}
         </h2>
         <CustomDatePickerDiv>
           <DatePicker
@@ -151,8 +288,41 @@ export default function Reservation() {
             picture={store.picture}
             allTime={store.allTime}
             noTime={store.noTime}
+            handleClickedTimeButton={handleClickedTimeButton}
           />
         ))}
+      </div>
+      <div style={{ border: '1px solid red', fontSize: '8px' }}>
+        {`${reservation.idx}/${reservation.storeName}/`}
+        {reservation.date !== null
+          ? `${reservation.date.getFullYear()} ${reservation.date.getMonth()}`
+          : null}
+        {`/${reservation.time}`}
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        {
+          reservationButton ? (
+            <Button
+              variant="contained"
+              style={{ position: 'fixed', bottom: '50px' }}
+            >
+              상담 예약하기
+            </Button>
+          ) : null
+          // <Button
+          //   variant="contained"
+          //   disabled
+          //   style={{ position: 'fixed', bottom: '50px' }}
+          // >
+          //   상담 예약하기
+          // </Button>
+        }
       </div>
     </div>
   );
--- frontend/src/pages/reservation/elements/StoreInfo.jsx
+++ frontend/src/pages/reservation/elements/StoreInfo.jsx
@@ -1,10 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import Rating from '@mui/material/Rating';
 import Button from '@mui/material/Button';
-
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { reservationDate, chosenReservation } from '../../../atom';
 
 export default function StoreInfo({
   idx,
@@ -17,65 +14,25 @@ export default function StoreInfo({
   picture,
   allTime,
   noTime,
+  handleClickedTimeButton,
 }) {
-  const chosenDate = useRecoilValue(reservationDate);
-  const [clickedButton, setClickedButton] = useState(null);
-  const [reservation, setReservation] = useRecoilState(chosenReservation);
-  useEffect(() => {
-    if (clickedButton !== null) {
-      clickedButton.style.backgroundColor = '#1976D2';
-      clickedButton.style.color = '#ffffff';
-    }
-  }, [clickedButton]);
-  const handleChosenReservation = (event) => {
-    // event.target.setProperty('variant', 'contained');
-    // 새로운 데이터 생성
+  const chooseTimeButton = (event) => {
     const data = {
       idx,
       storeName,
-      date: chosenDate,
+      date: null,
       time: event.target.innerText,
     };
-
-    // 이미 Recoil의 chosenReservation의 내용과 다르면
-    // Recoil의 chosenReservation 업데이트
-
-    // 같으면
-    // 1. chosenReservation 해제
-    // 2. event 담긴 버튼의 variant outline으로 변경
-
-    if (
-      // eslint-disable-next-line operator-linebreak
-      data.idx !== reservation.idx ||
-      // eslint-disable-next-line operator-linebreak
-      data.time !== reservation.time ||
-      // eslint-disable-next-line operator-linebreak
-      data.date.getFullYear() !== reservation.date.getFullYear() ||
-      // eslint-disable-next-line operator-linebreak
-      data.date.getMonth() !== reservation.date.getMonth() ||
-      // eslint-disable-next-line operator-linebreak
-      data.date.getDate() !== reservation.date.getDate()
-    ) {
-      //   alert('신규 등록해야 해!');
-      if (clickedButton !== null) {
-        clickedButton.style.backgroundColor = '#ffffff';
-        clickedButton.style.color = '#1976D2';
-      }
-      setClickedButton(() => event.target);
-      setReservation(data);
-      return;
-    }
-    alert('일치하는거 또 눌렀네!');
-    setReservation({
-      idx: null,
-      storeName: null,
-      date: null,
-      time: null,
-    });
+    handleClickedTimeButton(event, data);
   };
   return (
-    <div style={{ border: '1px solid blue' }}>
-      <div style={{ display: 'flex' }}>
+    <div style={{ borderBottom: '1px solid black' }}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
         <Picture src={picture} alt="" />
         <div>
           <span style={{ fontSize: '24px', fontWeight: 'bold' }}>
@@ -111,7 +68,7 @@ export default function StoreInfo({
             return (
               <Button
                 variant="outlined"
-                onClick={handleChosenReservation}
+                onClick={chooseTimeButton}
                 key={time}
                 style={{
                   margin: '0px 8px',
