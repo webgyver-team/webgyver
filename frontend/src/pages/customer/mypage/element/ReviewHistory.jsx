8012299980b4f51fbd79a073badc74b0e25744df
qorzi <qorzi00@gmail.com>
2023-01-30 17:37:05 +0900

    [ADD] : mypage - myinfo 추가

---
+++ frontend/src/pages/customer/mypage/element/ReviewHistory.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function MyInfo() {
+  return (
+    <Main>
+      <p>이름</p>
+    </Main>
+  );
+}
+
+const Main = styled.div`
+  width: 100%;
+`;
