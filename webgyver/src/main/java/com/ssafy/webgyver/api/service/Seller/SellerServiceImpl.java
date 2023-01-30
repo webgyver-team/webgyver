35ef402e30fd408eec96d40a4fbe89590c324189
repeater1384 <repeater1384@naver.com>
2023-01-30 10:56:41 +0900

    [ADD] feature/article branch ADD

--- webgyver/src/main/java/com/ssafy/webgyver/api/service/Seller/SellerServiceImpl.java
+++ webgyver/src/main/java/com/ssafy/webgyver/api/service/Seller/SellerServiceImpl.java
@@ -18,7 +18,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Service("SellerSevice")
+@Service("SellerService")
 @RequiredArgsConstructor
 public class SellerServiceImpl implements SellerService{
     final PasswordEncoder passwordEncoder;
