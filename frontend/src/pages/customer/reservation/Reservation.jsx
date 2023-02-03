f25c7401f0a398b4c1f32baa4fc3886bd2513494
YoonHwe <dbsghl123@naver.com>
2023-02-03 16:42:05 +0900

    [ADD] : 상담 예약 ajax 호출 data 생성

--- frontend/src/pages/customer/reservation/Reservation.jsx
+++ frontend/src/pages/customer/reservation/Reservation.jsx
@@ -10,6 +10,7 @@ import {
   reservationDate,
   chosenReservation,
   locateModalState,
+  categoryState,
 } from '../../../atom';
 import StoreInfo from './elements/StoreInfo';
 import { storeList } from './dummyData';
@@ -17,6 +18,7 @@ import { storeList } from './dummyData';
 export default function Reservation() {
   // 위치설정 모달 on/off
   const setLocateModalOpen = useSetRecoilState(locateModalState);
+  const categoryIdx = useRecoilValue(categoryState);
   const openLocateModal = () => setLocateModalOpen(true);
   // 예약 정보 {idx, storeName, date, time}
   const reservationNull = {
@@ -102,9 +104,21 @@ export default function Reservation() {
   useEffect(() => {
     // 주소 또는 선택 날짜가 바뀌었으면
     // storeList 갱신해야 함
+    const data = {
+      categoryIdx,
+      lat: location.latitude,
+      lng: location.longitude,
+      date: date.replaceAll('-', ''),
+    };
+    //   {
+    //     "categoryIdx": 1,
+    //     "lat": 41.40338,
+    //     "lng": 2.17403,
+    //     "date": "20230202"
+    // }
     // eslint-disable-next-line
-    console.log('[가게 정보] axios 호출 필요');
-  }, [location, date]);
+    console.log(`[${data}] axios 호출 필요`);
+  }, [location, date, categoryIdx]);
 
   const [type, setType] = useState(1);
 
