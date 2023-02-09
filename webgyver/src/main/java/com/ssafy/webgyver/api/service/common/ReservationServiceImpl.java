608590c48af15e4ea3a88e9cf4c0deb92988d730
repeater1384 <repeater1384@naver.com>
2023-02-09 15:58:29 +0900

    [ADD] : 화상통화 시작시 발생하는 오류 수정

--- webgyver/src/main/java/com/ssafy/webgyver/api/service/common/ReservationServiceImpl.java
+++ webgyver/src/main/java/com/ssafy/webgyver/api/service/common/ReservationServiceImpl.java
@@ -1,8 +1,6 @@
 package com.ssafy.webgyver.api.service.common;
 
-import com.ssafy.webgyver.api.response.common.category.CategoryListRes;
 import com.ssafy.webgyver.db.entity.Reservation;
-import com.ssafy.webgyver.db.repository.common.CategoryRepository;
 import com.ssafy.webgyver.db.repository.common.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,9 +14,9 @@ public class ReservationServiceImpl implements ReservationService {
     final ReservationRepository reservationRepository;
 
     @Override
-    public Reservation getReservation(long reservationIdx) {
+    public Optional<Reservation> getReservation(long reservationIdx) {
         System.out.println("서비스까지 들어옴");
-        return reservationRepository.findById(reservationIdx).get();
+        return reservationRepository.findById(reservationIdx);
     }
 
     public void updateReservation2Meet(long reservationIdx, LocalDateTime time) {
