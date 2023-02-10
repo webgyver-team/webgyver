aa4b5d6ba1afa8ec8bf208de50582dbaa1e2c272
repeater1384 <repeater1384@naver.com>
2023-02-10 11:18:42 +0900

    [MOD] : allTime 시간 포맷 이슈 해결, 고객상담정보종료조회 api 구현

---
+++ webgyver/src/main/java/com/ssafy/webgyver/api/response/customer/CustomerReservationEndInfoRes.java
@@ -0,0 +1,49 @@
+package com.ssafy.webgyver.api.response.customer;
+
+import com.ssafy.webgyver.common.model.response.DataResponseBody;
+import com.ssafy.webgyver.db.entity.Reservation;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+public class CustomerReservationEndInfoRes extends DataResponseBody {
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Response {
+        private String companyName;
+        private String sellerName;
+        private String content;
+        private Integer price;
+
+    }
+
+    public static CustomerReservationEndInfoRes of(Integer statusCode, String message) {
+        CustomerReservationEndInfoRes res = new CustomerReservationEndInfoRes();
+        res.setStatusCode(statusCode);
+        res.setMessage(message);
+        return res;
+
+    }
+
+    public static CustomerReservationEndInfoRes of(Integer statusCode, String message, Map<String, Object> response) {
+        CustomerReservationEndInfoRes res = new CustomerReservationEndInfoRes();
+
+        res.setStatusCode(statusCode);
+        res.setMessage(message);
+        res.getData().put("data", Response
+                .builder()
+                .companyName((String) response.get("companyName"))
+                .sellerName((String) response.get("sellerName"))
+                .content((String) response.get("content"))
+                .price(((int) response.get("price")))
+                .build()
+        );
+
+        return res;
+    }
+}
