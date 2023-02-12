9209d2712880bdb4ef9f42f4ddb6da26a0cebce8
김동준 <dev.jun125@gmail.com>
2023-02-13 05:02:48 +0900

    [MOD] : 스프링 시큐리티

--- webgyver/src/main/java/com/ssafy/webgyver/config/SecurityConfig.java
+++ webgyver/src/main/java/com/ssafy/webgyver/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.ssafy.webgyver.common.auth.CustomCustomerDetailService;
 import com.ssafy.webgyver.common.auth.CustomSellerDetailService;
 import com.ssafy.webgyver.common.auth.JwtAuthenticationFilter;
 import com.ssafy.webgyver.common.auth.SsafyUserDetailService;
+import com.ssafy.webgyver.common.util.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * 인증(authentication) 와 인가(authorization) 처리를 위한 스프링 시큐리티 설정 정의.
@@ -30,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    private UserService userService;
     private CustomCustomerDetailService customCustomerDetailService;
     private CustomSellerDetailService customSellerDetailService;
-
+    private JwtTokenUtil jwtTokenUtil;
     private final UserDetailServiceImpl userDetailServiceImpl;
     // Password 인코딩 방식에 BCrypt 암호화 방식 사용
     @Bean
@@ -62,10 +64,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 하지않음
                 .and()
-//                .addFilter(new JwtAuthenticationFilter(authenticationManager(), userDetailServiceImpl)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
+//                .addFilter(new JwtAuthenticationFilter(authenticationManager(), customCustomerDetailService)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
+//                .addFilter(new JwtAuthenticationFilter(authenticationManager(), customSellerDetailService)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
+//                .addFilter(new JwtAuthenticationFilter(jwtTokenUtil),
+//                        UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
-                .antMatchers("/api/v1/**/member/**").permitAll()
-                .antMatchers("/api/v1/common/**").permitAll()       //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
+                .antMatchers("/api/v1/**/member/**", "/api/v1/common/**").permitAll()
+                .antMatchers().permitAll()       //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
 //                .anyRequest().hasAnyRole("CUSTOMER", "PARTNER")
                 //                .antMatchers("/admin/**").hasAnyRole("Partner")
                 .and().cors();
